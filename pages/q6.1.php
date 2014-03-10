2b3e700afc1461036790ab4f3341c3f76bf4d48b
fredefox <fhi.1990@gmail.com>
2014-03-10 13:54:08 +0100

    Add pages directory including solutions to first question

---
+++ pages/q6.1.php
@@ -0,0 +1,18 @@
+<table>
+<?php
+foreach($DB->query("SELECT * FROM employee") as $row) {
+	// I wanted to use something like join on the resulting array
+	// to only automatically write all the html
+	// PDO::query unfortunately returns an array with duplicate values
+	// Example: array("0" -> "value", "key" -> "value");
+	$employee_name = $row[0];
+	$street = $row[1];
+	$city = $row[2];
+	print "<tr>";
+	print "<td>$employee_name</td>";
+	print "<td>$street</td>";
+	print "<td>$city</td>";
+	print "</tr>";
+}
+?>
+</table>
